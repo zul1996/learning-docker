02c43dd15e645c6b2dd59f4c23fc01096bfa48e0
unknown <mzulnewbie@gmail.com>
2024-06-10 16:13:41 +0700

    feat: Menambah konfigurasi golang

---
+++ my-dockerfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+
+		fmt.Println("Someone hit me ")
+		return c.SendString("The true measure of a shinobi is not how he lives, but how he dies. - Jiraiya👋!")
+	})
+
+	log.Fatal(app.Listen(":80"))
+
+}
